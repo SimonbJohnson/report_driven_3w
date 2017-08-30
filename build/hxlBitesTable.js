521c0adc3ec0e1e058884fc2df332df38a7c3bc6
SJohnson <simonbjohnson@gmail.com>
2017-08-30 13:04:07 +0100

    adding build for demo

---
+++ build/hxlBitesTable.js
@@ -0,0 +1,12 @@
+hxlBites._tableBites = [
+{
+'id':'table0001',
+'type':'type',
+'subType':'long',
+'ingredients':[{'name':'who','tags':['#org']},{'name':'what','tags':['#activity','#sector']},{'name':'where','tags':['#country-code','#region-code','#adm1-code','#adm2-code','#adm3-code','#adm4-code']}],
+'criteria':['what > 2','who > 2', 'where > 2'],
+'variables': ['what', 'who', 'where'],
+'table':'rows(10)',
+'priority': 5,
+}
+]
